4be45e9943950e2576554b60daf37fda9c315010
Adrian Valencia <valenciaad@uninorte.edu.co>
2020-12-05 20:10:33 -0500

    se cambia pagina de blog para que accepte parametros

--- app.py
+++ app.py
@@ -16,9 +16,13 @@ def panelBlog():
     return render_template('panelBlog.html')
 
 
-@app.route('/paginaBlog')
+@app.route('/paginaBlog/')
+@app.route('/paginaBlog/<blogId>')
 def paginaBlog():
-    return render_template('paginaBlog.html')
+    contenidoBlog = [{"contenido":"Contenido del blog...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    "titulo":"Titulo del blog"}]
+    print(contenidoBlog[0]["titulo"])
+    return render_template('paginaBlog.html',contenidoBlog=contenidoBlog)
 
 
 @app.route('/resultadoBusqueda')
