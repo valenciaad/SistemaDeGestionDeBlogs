f4cfc8124013bee8c73f4926a188f8c0dd0181eb
Adrian Valencia <valenciaad@uninorte.edu.co>
2020-12-19 09:04:21 -0500

    archivo  de gnunicor

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
-                            import os
-
+import os
 import flask
 
 
 from classes import db,ma,login_manager
 from datetime import timedelta
 from views import pages
+def app ():
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app = flask.Flask(__name__)
+    app.secret_key = os.urandom(24)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'baseDatos.db')
+    app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=10)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app = flask.Flask(__name__)
-app.secret_key = os.urandom(24)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'baseDatos.db')
-app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=10)
 
+    login_manager.init_app(app)
+    db.init_app(app)
+    ma.init_app(app)
+    app.register_blueprint(pages)
 
-login_manager.init_app(app)
-db.init_app(app)
-ma.init_app(app)
-app.register_blueprint(pages)
-
-#if __name__ == '__main__':
-    #app.run(port=80,debug=True)
-#    app.run(host='0.0.0.0', port=443, ssl_context=('micertificado.cer', 'llaveprivada.pem'))
+    #if __name__ == '__main__':
+        #app.run(port=80,debug=True)
+    return app.run(host='0.0.0.0', port=443, ssl_context=('micertificado.cer', 'llaveprivada.pem'))
 
